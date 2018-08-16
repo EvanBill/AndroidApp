4b2b233442be15e4e5e1a94e94e586fa411b67ee
zhangzhanhua <zzh1028752529@gmail.com>
2018-08-16 23:34:33 +0800

    忽略文件1

--- app/src/main/java/com/example/zhang/ui/MainActivity.java
+++ app/src/main/java/com/example/zhang/ui/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.zhang.ui;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.zhang.R;
 import com.example.zhang.base.BaseActivity;
