35e4924f4182c08fe2b45c53d9687c22e039822b
zhangzhanhua <zzh1028752529@gmail.com>
2019-07-12 18:01:07 +0800

    FileProvider适配Android7.0

---
+++ app/src/main/java/com/example/zhang/mvp/contract/FileProviderContract.java
@@ -0,0 +1,9 @@
+package com.example.zhang.mvp.contract;
+
+import com.example.zhang.base.BaseView;
+
+public interface FileProviderContract {
+    interface IFileProviderContractView extends BaseView {
+
+    }
+}
